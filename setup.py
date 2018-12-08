54ca99631ee2d4d5f9f83e69cc33facad8e4e177
Tom Aldcroft <taldcroft@gmail.com>
2018-12-08 17:44:34 -0500

    Update version to 4.2 + PEP8 cleanup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(name='Ska.arc5gl',
-      author = 'Tom Aldcroft',
+      author='Tom Aldcroft',
       description='Use arc5gl to access the Chandra archive',
-      author_email = 'aldcroft@head.cfa.harvard.edu',
-      py_modules = ['Ska.arc5gl'],
-      version='3.1.1',
+      author_email='taldcroft@cfa.harvard.edu',
+      py_modules=['Ska.arc5gl'],
+      version='4.2',
       zip_safe=False,
       packages=['Ska'],
-      package_dir={'Ska' : 'Ska'},
+      package_dir={'Ska': 'Ska'},
       package_data={}
       )
